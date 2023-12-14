9f00f24c27dfec83e41f798d80d1b642ffcddc62
Nihat Duysak <nihatduysak@Nihats-MacBook-Air.local>
2023-12-15 00:21:48 +0300

    güncellendi v1.2

--- src/assets/components/FinishQuiz.jsx
+++ src/assets/components/FinishQuiz.jsx
@@ -5,32 +5,34 @@ import data from '/data.json';
 export default function FinishQuiz({ score, currentQuiz }) {
     return (
         <>
-            <div className="questionHeader">
-                <div className="questionTitle">
-                    <img src={currentQuiz.icon} alt={currentQuiz.title} />
-                    <p>{currentQuiz.title}</p>
-                </div>
-                <div className="div">
-                    <Header />
-                </div>
-            </div>
-            <div className="finishQuizContainer">
-                <div className="quizComplete">
-                    <h2 className="quizStatus">Quiz completed</h2>
-                    <h2 className="scoreText"><b>You scored...</b></h2>
-                </div>
-                <div className="scoreBoard">
-                    <div className="quizScore">
-                        <div className="access">
-                            <img src={currentQuiz.icon} alt={currentQuiz.title} />
-                            <p className="topic">{currentQuiz.title}</p>
-                        </div>
-                        <span>{score}</span>
-                        <p>out of 10</p>
+            <div className="container">
+                <div className="questionHeader">
+                    <div className="questionTitle">
+                        <img src={currentQuiz.icon} alt={currentQuiz.title} />
+                        <p>{currentQuiz.title}</p>
                     </div>
-                    <div className="playAgainBtn">
-                        <button><p>Play Again</p></button>
-                    </div> 
+                    <div className="div">
+                        <Header />
+                    </div>
+                </div>
+                <div className="finishQuizContainer">
+                    <div className="quizComplete">
+                        <h2 className="quizStatus">Quiz completed</h2>
+                        <h2 className="scoreText"><b>You scored...</b></h2>
+                    </div>
+                    <div className="scoreBoard">
+                        <div className="quizScore">
+                            <div className="access">
+                                <img src={currentQuiz.icon} alt={currentQuiz.title} />
+                                <p className="topic">{currentQuiz.title}</p>
+                            </div>
+                            <span>{score}</span>
+                            <p>out of 10</p>
+                        </div>
+                        <div className="playAgainBtn">
+                            <button><p>Play Again</p></button>
+                        </div> 
+                    </div>
                 </div>
             </div>
         </>
